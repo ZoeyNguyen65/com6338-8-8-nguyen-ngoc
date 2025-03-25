c57cf1dcdb978c3a13f99d1d8ffff6c693f0083a
ZoeyNguyen65 <hanhngoc9956@gmail.com>
2025-03-24 23:41:57 -0500

    modify index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Select the form, input, and weather section elements
+//Select the form, input, and weather section elements//
 const form = document.querySelector('form');
 const weatherSection = document.querySelector('#weather');
 const inputField = document.querySelector('input[name="search"]');
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -9,22 +10,28 @@ form.addEventListener('submit', (event) => {
     const location = inputField.value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=be6ccdb952d3f470cf48e6994f68c6e7`;
     
-    inputField.value = ''; // Reset input field value
-    weatherSection.innerHTML = ''; // Reset weather section content
-    weatherSection.style.display = 'none'; // Reset display property
+    inputField.value = ''; //To reset input field value//
+    weatherSection.innerHTML = ''; //To reset weather section content//
+    weatherSection.style.display = 'none'; //To reset display property//
 
   
-
   fetch(url)
   .then(response => response.json())
   .then(data => {
   if (data.cod === "404") {
-  const weatherHTML = `<h2>Location not found</h2>`;
+  const weatherHTML = `<h2>Location Not Found</h2>`;
   weatherSection.innerHTML = weatherHTML;
   weatherSection.style.display = 'block';
+
   } else {
     const lat = data.coord.lat;
     const lon = data.coord.lon;
+    var date = new Date(/* milliseconds go here */)
+    var timeString = date.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: '2-digit'
+    })
+
     const weatherHTML = `
       <h2>${data.name}, ${data.sys.country}</h2>
       <a href="https://www.google.com/maps/search/?api=1&query=${lat},${lon}" target="_blank">Click to view map</a>
@@ -34,10 +41,12 @@ form.addEventListener('submit', (event) => {
       <p>Current: ${data.main.temp}°F</p>
       <p>Feels like: ${data.main.feels_like}°F</p>
       <br>
-      <p>Last updated: ${new Date(data.dt * 1000).toLocaleTimeString()}</p>
+      <p>Last updated: ${new Date(data.dt * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' })}</p>
+      
     `;
     weatherSection.innerHTML = weatherHTML;
     weatherSection.style.display = 'block';
   }})
+
   .catch(error => console.error('Error:', error));
 });
